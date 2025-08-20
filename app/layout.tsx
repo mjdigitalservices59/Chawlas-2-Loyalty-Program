d1f04159178c5d0ffa528c3cb6bdf6a1128fce1e
mjdigitalservices59 <mjdigitalservices59@gmail.com>
2025-08-20 19:09:03 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = { title: 'Chawlas2 Loyalty' };
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body style={{margin:0}}>{children}</body>
+    </html>
+  );
+}
